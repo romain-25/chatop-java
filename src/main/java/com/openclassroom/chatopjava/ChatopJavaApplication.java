98f1bb25443eb3a2b20554e6fd0ae30454c9c166
romain-25 <romain.rouabah@gmail.com>
2024-05-01 14:36:47 +0200

    creat project

---
+++ src/main/java/com/openclassroom/chatopjava/ChatopJavaApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.chatopjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatopJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openclassroom/chatopjava/ChatopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroom.chatopjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
